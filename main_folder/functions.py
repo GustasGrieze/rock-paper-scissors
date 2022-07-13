a6f734d2f042fdbe413008fb57dbc028b90b9dba
GustasGrieze <106992606+GustasGrieze@users.noreply.github.com>
2022-07-13 21:54:42 +0300

    Update functions.py

--- main_folder/functions.py
+++ main_folder/functions.py
@@ -10,22 +10,18 @@ def print_user_choice(choice: int, rock: str, paper: str, scissors: str) -> None
         
 def select_computer_choice(rock: str, paper: str, scissors: str) -> int:
     game_images = [rock, paper, scissors]
-    amount_of_images = len(game_images)
-    image_chosen = random.randint(0, amount_of_images - 1)
+    image_chosen = random.randint(0, len(game_images) - 1)
     print(game_images[image_chosen])
     return image_chosen
 
 def user_won(choice: int, image_chosen: int) -> bool:
-    if (choice == 0 and image_chosen == 2) or (choice == 1 and image_chosen == 0) or (choice == 2 and image_chosen == 1):
-        return True
+    return (choice == 0 and image_chosen == 2) or (choice == 1 and image_chosen == 0) or (choice == 2 and image_chosen == 1):
 
 def user_lost(choice: int, image_chosen: int) -> bool:
-    if (choice == 1 and image_chosen == 2) or (choice == 0 and image_chosen == 1) or (choice == 2 and image_chosen == 0):
-        return True
+    return (choice == 1 and image_chosen == 2) or (choice == 0 and image_chosen == 1) or (choice == 2 and image_chosen == 0):
 
 def user_tied(choice: int, image_chosen: int) -> bool:
-    if choice == image_chosen:
-        return True
+    return choice == image_chosen:
 
 def write_information_to_file(data: str) -> None:
     with open("../results.txt", 'a') as f:
@@ -35,5 +31,5 @@ def calculate_user_win_rate(games_played: int, games_won: int, games_tied: int) 
     games_played = games_played - games_tied
     try:
         return round(games_won / games_played * 100)
-    except ZeroDivisionError:
+    except Exception as e:
         return 0
