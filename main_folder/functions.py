6b182f72629dc5df42bc5da2608c35b9a7215c97
GustasGrieze <106992606+GustasGrieze@users.noreply.github.com>
2022-07-13 14:17:52 +0300

    Update functions.py

--- main_folder/functions.py
+++ main_folder/functions.py
@@ -1,5 +1,20 @@
 import random
 
+def print_user_choice(choice: int, rock: str, paper: str, scissors: str) -> None:
+    if choice == 0:
+        print(f"\nYou chose: \n{rock}")
+    elif choice == 1:
+        print(f"\nYou chose: \n{paper}")
+    elif choice == 2:
+        print(f"\nYou chose: \n{scissors}")
+        
+def select_computer_choice(rock: str, paper: str, scissors: str) -> int:
+    game_images = [rock, paper, scissors]
+    amount_of_images = len(game_images)
+    image_chosen = random.randint(0, amount_of_images - 1)
+    print(game_images[image_chosen])
+    return image_chosen
+
 def user_won(choice: int, image_chosen: int) -> bool:
     if (choice == 0 and image_chosen == 2) or (choice == 1 and image_chosen == 0) or (choice == 2 and image_chosen == 1):
         return True
@@ -22,18 +37,3 @@ def calculate_user_win_rate(games_played: int, games_won: int, games_tied: int) 
         return round(games_won / games_played * 100)
     except ZeroDivisionError:
         return 0
-
-def print_user_choice(choice: int, rock: str, paper: str, scissors: str) -> None:
-    if choice == 0:
-        print(f"\nYou chose: \n{rock}")
-    elif choice == 1:
-        print(f"\nYou chose: \n{paper}")
-    elif choice == 2:
-        print(f"\nYou chose: \n{scissors}")
-
-def select_computer_choice(rock: str, paper: str, scissors: str) -> int:
-    game_images = [rock, paper, scissors]
-    amount_of_images = len(game_images)
-    image_chosen = random.randint(0, amount_of_images - 1)
-    print(game_images[image_chosen])
-    return image_chosen
